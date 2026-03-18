e64a82eb953fd8a02cb5b35ca2ff6b3c0cf14aa7
Pooja <poojasaini2821@gmail.com>
2026-03-18 22:29:14 +0530

    error resolve

--- db.js
+++ db.js
@@ -2,14 +2,13 @@ import { MongoClient } from "mongodb";
 
 let isDbConnected = null;
 
-const CLUSTER_URL = "mongodb+srv://poojasaini2823671:BySx0l2CnOqxTXkV@cluster0.jrcqais.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-const client = new MongoClient(CLUSTER_URL)
+const CLUSTER_URL = process.env.MONGO_URI;
+const client = new MongoClient(CLUSTER_URL);
 
 //async make a function asynchronous, we can use await
 async function connectToDatabase(dbName) {
     if (isDbConnected === null) {
         try {
-            //await waits for a mongodb connection to finish before moving to the next line in an async function
             await client.connect();
             isDbConnected = client.db(dbName)
             console.log("database connected successfully")
@@ -19,9 +18,8 @@ async function connectToDatabase(dbName) {
     }
     else {
         console.log("database already connected")
-
     }
     return isDbConnected;
-
 }
+
 export default connectToDatabase;
--- server.js
+++ server.js
@@ -1,86 +1,96 @@
+import './config.js';  
+
 import express from 'express'
 import connectToDatabase from './db.js'
 import cors from 'cors'
 
-const app= express()
-const port=3000
+const app = express()
+const port = process.env.PORT || 3000;
 let db;
 
 app.use(express.json())
+
 app.use(cors({
-    origin:"https://todo-app-frontend-2xqa.onrender.com"
+    origin: "https://todo-app-frontend-2xqa.onrender.com"
 }));
 
-app.listen(port,async()=>{
-    db=await connectToDatabase('todo-project-db')
+app.listen(port, async () => {
+    db = await connectToDatabase('todo-project-db')
     console.log(`todo app backend server started at port ${port}`)
 })
 
-app.get('/test',(req,res)=>{
+app.get('/test', (req, res) => {
     res.send('API is up!!')
 })
 
-app.post('/create-todo',async(req,res)=>{
-    try{
-        let body=req.body;
+app.post('/create-todo', async (req, res) => {
+    try {
+        let body = req.body;
         await db.collection('todo').insertOne(body);
-        res.status(201).json({msg:"todo inserted successfully"})
-    }catch(error){
-        res.status(500).json({msg:["internal server occur"]})
+        res.status(201).json({ msg: "todo inserted successfully" })
+    } catch (error) {
+        res.status(500).json({ msg: ["internal server occur"] })
     }
 })
 
-app.get('/read-todos',async(req,res)=>{
-    try{
+app.get('/read-todos', async (req, res) => {
+    try {
         let todolist = await db.collection('todo').find().toArray();
         res.status(200).json(todolist)
-    }catch(error){
-        res.status(500).json({msg:["internal server occur"]})
+    } catch (error) {
+        res.status(500).json({ msg: ["internal server occur"] })
     }
 })
 
-app.get('/read-todo',async(req,res)=>{
-    try{
-        let queryTodoId=req.query.todoId;
-        let todo = await db.collection('todo').findOne({'todoId':queryTodoId});
+app.get('/read-todo', async (req, res) => {
+    try {
+        let queryTodoId = req.query.todoId;
+        let todo = await db.collection('todo').findOne({ 'todoId': queryTodoId });
         res.status(200).json(todo)
-    }catch(error){
+    } catch (error) {
         res.status(500).json({
-            msg:"internal server occur",
-            error:error.message
+            msg: "internal server occur",
+            error: error.message
         })
     }
 })
 
-app.patch('/update-todo',async(req,res)=>{
-    try{
-        let queryTodoId=req.query.todoId;
-        let reqbody= req.body;
-        let result = await db.collection('todo').updateOne({'todoId':queryTodoId},{$set:reqbody})
-        if(result.matchedCount===0){
-            res.status(404).json({msg:'todo not found'})
-        }else
-            res.status(201).json({msg:'todo updated successfully'})
-    }catch(error){
-            res.status(500).json({
-                msg:"internalserver occur",
-                error:error.message
-           })
+app.patch('/update-todo', async (req, res) => {
+    try {
+        let queryTodoId = req.query.todoId;
+        let reqbody = req.body;
+        let result = await db.collection('todo').updateOne(
+            { 'todoId': queryTodoId },
+            { $set: reqbody }
+        )
+
+        if (result.matchedCount === 0) {
+            res.status(404).json({ msg: 'todo not found' })
+        } else {
+            res.status(201).json({ msg: 'todo updated successfully' })
+        }
+    } catch (error) {
+        res.status(500).json({
+            msg: "internalserver occur",
+            error: error.message
+        })
     }
 })
 
-app.delete('/delete-todo',async(req,res)=>{
-    try{
-        let queryTodoId=req.query.todoId;
-        let result = await db.collection('todo').deleteOne({'todoId':queryTodoId})
-        if(result.deletedCount===0){
-            res.status(404).json({msg:'todo not found'})
-        }else
-            res.status(201).json({msg:'todo deleted'})
-    }catch(error){
-            res.status(500).json({
-                msg:"internalserver occur",
-                error:error.message
-           })
+app.delete('/delete-todo', async (req, res) => {
+    try {
+        let queryTodoId = req.query.todoId;
+        let result = await db.collection('todo').deleteOne({ 'todoId': queryTodoId })
+
+        if (result.deletedCount === 0) {
+            res.status(404).json({ msg: 'todo not found' })
+        } else {
+            res.status(201).json({ msg: 'todo deleted' })
+        }
+    } catch (error) {
+        res.status(500).json({
+            msg: "internalserver occur",
+            error: error.message
+        })
     }
 })
